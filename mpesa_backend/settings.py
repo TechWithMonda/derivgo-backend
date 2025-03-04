ad9a2f6024cadd6d40010947ca7f60ae02dbbbf6
MondaSamuel <samwelmonda48@gmail.com>
2025-03-05 01:40:55 +0300

    added callback

--- mpesa_backend/settings.py
+++ mpesa_backend/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3w)du5q*9#%5%o!ed%mpg9$!79t&658$qe-ar)*tgc-2ca=cmw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
--- mpesa_backend/urls.py
+++ mpesa_backend/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import mpesa_payment, mpesa_callback, home
+from .views import home, mpesa_payment, mpesa_callback
 
 urlpatterns = [
-    path('', home, name='home'),  # Add this line
+    path('', home, name='home'),
     path('api/payment/', mpesa_payment, name='mpesa_payment'),
-    path('api/mpesa-callback/', mpesa_callback, name='mpesa_callback'),
+    path('api/mpesa-callback/', mpesa_callback, name='mpesa_callback'),  # Add this line
 ]
